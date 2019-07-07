f40cc609208b5f16e18a8ee161093aa7d1cce617
Iman Gure <imangu99@gmail.com>
2019-07-06 20:23:25 -0700

    updating

--- liri.js
+++ liri.js
@@ -1,162 +1,137 @@
-// read and set any environment variables with the dotenv package:
+// Require in the .env file that holds my private Spotify keys
 require("dotenv").config();
 
-var keys = require("./keys.js");
+// Declare the variables for my required dependencies 
+let keys = require("./keys");
+let Spotify = require("node-spotify-api");
+let spotify = new Spotify(keys.spotify);
+const axios = require("axios");
+const moment = require("moment");
+const fs = require("fs");
 
-// 1 define a terminal global app name
-// 2 define commands
-// 3 extract the argument
+// Declare the variables that will hold the user input
+let choice = process.argv[2];
+let value = process.argv.slice(3).join(" ");
 
-// liri song living on a prayer
-// var songName = getSongName();
-
-// send song name to spotifiy api
-// display the data on the screen
-
-    
-    // Configure APIs
-    
-var keys = require("./keys.js");
-
-// Twitter
-var Twitter = require("twitter");
-var twitter = new Twitter(keys.twitter);
-
-// Spotify
-var Spotify = require("node-spotify-api");
-var spotify = new Spotify(keys.spotify);
-
-// OMDB
-var request = require("request");
-
-// File System
-var fs = require("fs");
-
-// Operating System (for end-of-line)
-var os = require("os");
-
-
-    // Initialize
-    
-process.stdout.write("\033c");
-
-// Create a log file if it does not exist
-var file_log = "log.txt";
-
-if (!fs.existsSync(file_log)) {
-    fs.writeFile(file_log, "", error => {
-        if (error) {
-            console.log(`Error in creating "${file_log}"\n${error}\n\n\n`);
-            return;
+// Setting up the function for the 4 user search options
+function concertThis(value) {
+    axios.get(`https://rest.bandsintown.com/artists/${value}/events?app_id=codingbootcamp`).then(
+        function (response) {
+            const date = moment(response.data[0].datetime).format('MM/DD/YYYY');
+            console.log(`Venue name: ${response.data[0].venue.name}`);
+            console.log(`Venue location: ${response.data[0].venue.city}`);
+            console.log(`Date: ${date}`);
+            console.log(`===================================================================================`);
         }
-    });
-}
+    );
+};
 
-var option = process.argv[2];
-var title = process.argv.slice(3).join(" ");
+// Function to search on the Spotify API
+function spotifyThis(value) {
+    spotify.search({
+        type: 'track',
+        query: `${value}`,
+        limit: 3
+    })
+        .then(function (response) {
 
-mainMenu(option, title);
-
-    
-    // Menu options
-    
-function mainMenu(option = "", title) {
-    switch (option.toLowerCase()) {
-        case "my-tweets":
-            getTweets();
-            break;
-
-        case "spotify-this-song":
-            getSong((title) ? title : "The Sign");
-            break;
-
-        case "movie-this":
-            getMovie((title) ? title : "Mr. Nobody");
-            break;
-
-        case "do-what-it-says":
-            doWhatItSays();
-            break;
-
-        default:
-            saveOutput(`Error:\n"${option}" is a not valid command.\nPlease select "my-tweets", "spotify-this-song", "movie-this", or "do-what-it-says".\n\n\n`);
-
-    }
-}
-
-
-function getTweets() {
-    var parameters = {
-        "count": 20,
-        "screen_name": "BobBarker000000"
-    };
-
-    twitter.get("statuses/user_timeline", parameters, (error, tweets, response) => {
-        if (error) {
-            saveOutput(`Error in calling "Twitter"\n${error}\n\n\n`);
-            return;
-        }
-
-            
-            // Write to terminal and file
-            
-        let output = "My Tweets\n";
-
-        output += addSeparator();
-
-        tweets.forEach(t => {
-            // Extract date information
-            [, month, day, time, , year] = t.created_at.split(" ");
-            [hour, minute] = time.split(":").map(x => parseInt(x, 10));
-
-            // Format the time stamp
-            var timeStamp = `${month} ${day} ${year}, ${hour % 12}:${minute} ${(hour < 12) ? "AM" : "PM"}`;
-
-            output += `@${t.user.screen_name} · ${timeStamp}\n"${t.text}"\n\n`;
+            let a = response.tracks.items;
+            // console.log(a)
+            a.forEach(songs => {
+                const artist = `Artist(s): ${songs.artists[0].name}`;
+                const songName = `Song name: ${songs.name}`;
+                const preview = `Preview: ${songs.preview_url}`;
+                const albumName = `Album name: ${songs.album.name}`;
+                const line = `===================================================================================`;
+                console.log(artist)
+                console.log(songName);
+                console.log(preview)
+                console.log(albumName)
+                console.log(line)
+            });
         });
-
-        output += addSeparator() + "\n";
-
-        saveOutput(output);
-    });
 }
 
+// Function to search on the OMDB API
+function movieThis(value) {
+    if (!value) {
+        value = "mr nobody";
+        movieThis(value);
+    } else {
+        // Then run a request with axios to the OMDB API with the movie specified and log the response
+        axios.get('http://www.omdbapi.com/?t=${value}=&apikey=1a439abf').then( 
+        function (response) {
+                console.log(`Title: ${response.data.Title}`);
+                console.log(`Year: ${response.data.Year}`);
+                console.log(`IMDB rating: ${response.data.imdbRating}`);
+                console.log(`Rotten Tomatoes: ${response.data.Ratings[1].Value}`);
+                console.log(`Country: ${response.data.Country}`);
+                console.log(`Language: ${response.data.Language}`);
+                console.log(`Plot: ${response.data.Plot}`);
+                console.log(`Actors: ${response.data.Actors}`);
+                console.log(`===================================================================================`);
+            }
+        );
+    }
+};
 
-function getSong(title) {
-    var parameters = {
-        "type": "track",
-        "query": title,
-        "limit": 1
-    };
-
-    spotify.search(parameters, (error, data) => {
+// Function to run a random search for whatever is written in our random.txt file
+function doWhatItSays() {
+    // Use fs to read the random.txt file and run the search
+    fs.readFile("random.txt", "utf8", function (error, data) {
+        // If the code experiences any errors it will log the error to the console.
         if (error) {
-            saveOutput(`Error in calling "Spotify"\n${error}\n\n\n`);
-            return;
+            return console.log(error);
         }
+        // We will then run the search
+        console.log(data);
 
-        // For simplicity, we assume that Spotify always finds the right song
-        var song = data.tracks.items[0];
+        // Then split it by commas (to make it more readable)
+        let dataArr = data.split(",");
 
-        // Display all artists
-        var artists = song.artists.map(a => a.name);
+        // We will then re-display the content as an array for later use.
+        let choice = dataArr[0];
+        let value = dataArr[1];
 
-            
-            // Write to terminal and file
-            
-        let output = "Spotify This Song\n";
-
-        output += addSeparator();
-
-        output += `Artists      : ${artists.join(", ")}\n`;
-        output += `Album        : ${song.album.name}\n`;
-        output += `Track        : ${song.name}\n`;
-        output += `Preview link : ${song.preview_url}\n\n`;
-
-        output += addSeparator() + "\n";
-
-        saveOutput(output);
+        function doSearch() {
+            if (choice === "concert-this") {
+                concertThis(value);
+            } else if (choice === "spotify-this-song") {
+                spotifyThis(value);
+            } else if (choice === "movie-this") {
+                movieThis(value);
+            }
+        }
+        doSearch();
     });
-}
+};
 
+// Main function that runs the other functions based on user input
+function liriSearch() {
+    if (choice === "concert-this") {
+        concertThis(value);
+    } else if (choice === "spotify-this-song") {
+        if (value) {
+            spotifyThis(value);
+        } else {
+            value = "The sign by ace of base";
+            spotifyThis(value);
+        }
+    } else if (choice === "movie-this") {
+        movieThis(value);
+    } else if (choice === "do-what-it-says") {
+        doWhatItSays();
+    } else {
+        console.log(`===================================================================================`);
+        console.log(
+            `First type "node liri.js" followed by any of these commands: 
+        \n concert-this "the name of an artist who is touring"
+        \n spotify-this-song "any song title"
+        \n movie-this "any movie title"
+        \n "do-what-it-says"`);
+        console.log(`===================================================================================`);
+    }
+};
+
+// Calling the main LIRI search function to begin the app
 
